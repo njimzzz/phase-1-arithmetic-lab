2a463d2c010c5b6a60fa262718e0d76e74d16bde
Kyle <kylejrgurlad@gmail.com>
2024-03-19 11:28:10 +0300

    completed task

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2;
 
-// Write your code here
+const num3 = 24
+const num4 = 10
+const mod = 24 % 10
+
+let random = 0;
+++random 
+
+let max = 20
+Math.max(5, 10, 20)
+
+
